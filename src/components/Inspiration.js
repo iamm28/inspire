bdc6ebc9a560ba29d9b2b08daaa3470de4c75d1a
iamm28 <alisonmackay246@gmail.com>
2018-04-22 20:26:27 -0400

    can add link to video or web image

--- src/components/Inspiration.js
+++ src/components/Inspiration.js
@@ -2,24 +2,53 @@ import React, {Component} from 'react'
 import YouTube from 'react-youtube';
 
 class Inspiration extends Component {
+  list() {
+    if (this.props.video && this.props.webImage) {
+      const vid = this.props.video.split('v=')[1]
+      const opts = {
+        height: '390',
+        width: '640',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+          autoplay: 0
+        }
+      };
+      return (
+        <div>
+          <YouTube
+            videoId={vid}
+            opts={opts}
+            onReady={this._onReady}
+          />
+          <img src={`${this.props.webImage}`} height='390' width='640'/>
+        </div>
+      )
+    } else if (this.props.video) {
+     const vid = this.props.video.split('v=')[1]
+     const opts = {
+       height: '390',
+       width: '640',
+       playerVars: { // https://developers.google.com/youtube/player_parameters
+         autoplay: 0
+       }
+     };
+     return (
+       <div>
+         <YouTube
+           videoId={vid}
+           opts={opts}
+           onReady={this._onReady}
+         />
+       </div>
+     )
+   } else if (this.props.webImage) {
+   //const img = this.props.webImage
+    return (<img src={`${this.props.webImage}`} height='390' width='640'/>)
+  }
+  }
   render() {
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 0
-      }
-    };
-    const vid = this.props.video.split('v=')[1]
-    const img = this.props.webImage 
-    return (
+    return(
       <div>
-      <YouTube
-        videoId={vid}
-        opts={opts}
-        onReady={this._onReady}
-      />
-      <img src={img} height='390' width='640'/>
+        {this.list()}
       </div>
     )
   }
--- src/reducers/manageInspiration.js
+++ src/reducers/manageInspiration.js
@@ -5,7 +5,6 @@ export default function manageTodo(state = {
     case 'ADD_VIDEO':
       return { ...state, videos: state.videos.concat(action.video)}
     case 'ADD_WEB_IMAGE':
-      debugger
       return { ...state, webImages: state.webImages.concat(action.webImage)}
     default:
       return state;
